5b7c02c0a02346ebf7fac6a7f010b620eb1094e2
angelo bessolo <angelobessolo@gmail.com>
2025-03-15 10:23:04 -0500

    cambio en environments

--- environments/environments.ts
+++ environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    // baseUrl: 'http://localhost:3200'
+    // baseUrl: 'http://localhost:3000'
     baseUrl: 'https://todo-app-back-production.up.railway.app'
     
 }
